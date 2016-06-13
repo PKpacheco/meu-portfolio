3f22f1965dbf0a46cac304ad847a4db286a4a733
Juscilan Moreto <juscilan.moreto_desenv@hotmail.com>
2016-06-13 11:36:36 -0300

    Fix Language and TimeZone

--- mysite/settings.py
+++ mysite/settings.py
@@ -86,9 +86,9 @@ DATABASES = {
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Sao_Paulo'
 
 USE_I18N = True
 
